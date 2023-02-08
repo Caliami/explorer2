80ab4d3f40ca0e1775638768064d6c17bb430b54
caliami <106258560+Caliami@users.noreply.github.com>
2023-02-08 17:20:21 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+let burger = document.querySelector('.mobile-menu');
+let menu =document.querySelector('.menu');
+burger.addEventListener('click', function() {
+    menu.classList.toggle('active')
+}) 
